732ef8685c4c5d9ac1b553a36082f0dcffbebadd
badger <badger@badger.local>
2022-04-24 19:20:50 +0300

    47. Tax rate saga

--- redux-saga-cart-begin/src/sagas/index.js
+++ redux-saga-cart-begin/src/sagas/index.js
@@ -3,3 +3,4 @@ export {fetchCartSaga} from "./fetchCartSaga";
 export {itemDetailsSaga} from "./itemDetailsSaga";
 export {itemQuantitySaga} from "./itemQuantitySaga";
 export {shippingSaga} from "./shippingSaga";
+export {taxRate} from "./taxRateSaga";
